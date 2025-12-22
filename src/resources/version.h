8d5813eb5c785779c306f017ea84cef75cdbe8a1
Joshhhuaaa <41166025+Joshhhuaaa@users.noreply.github.com>
2025-12-21 19:53:18 -0600

    Update to Enhanced v1.4

--- src/resources/version.h
+++ src/resources/version.h
@@ -9,8 +9,8 @@
 #define DISCORD_URL "https://discord.com/invite/k6mZJcfjSh"
 
 #define VERSION_MAJOR     1
-#define VERSION_MINOR     3
-#define VERSION_PATCH     1
+#define VERSION_MINOR     4
+#define VERSION_PATCH     0
 
 
 #define STRINGIFY_HELPER(x) #x
